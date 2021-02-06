99b82696ac84cf0cbaf5d0b3cf170505d52bab0d
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-02-07 00:23:17 +0600

    all world added with deatils

--- app.js
+++ app.js
@@ -10,8 +10,25 @@ const displayCountries = countries => {
         const countryInfo = `
         <h3>${country.name}</h3>
         <p>${country.capital}</p>
+        <button onclick="displayCountryDetails('${country.name}')"> Details </button>
         `;
         countryDiv.innerHTML = countryInfo;
         countriesContainer.appendChild(countryDiv)
     });
+}
+const displayCountryDetails = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => renderCountryInfo(data[0]));
+}
+const renderCountryInfo = country => {
+    const countryDiv = document.getElementById('countryDetails');
+    countryDiv.innerHTML = `
+    <h1>${country.name}</h1>
+    <p>Population: ${country.population}</p>
+    <p>Area: ${country.area} sq km. </p>
+    <img src="${country.flag}">
+    `
+
 }
